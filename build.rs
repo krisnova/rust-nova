8a7a9e19814237118c735fadbcf7a57a199e4f1c
Kris Nóva <kris@nivenly.com>
2022-08-15 12:55:55 -0400

    Automatic commit from github.com/kris-nova/bin
    Signed-off-by: Kris Nóva <kris@nivenly.com>

--- build.rs
+++ build.rs
@@ -15,6 +15,6 @@
 \*===========================================================================*/
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/helloworld.proto")?;
+    tonic_build::compile_protos("proto/echo.proto")?;
     Ok(())
 }
